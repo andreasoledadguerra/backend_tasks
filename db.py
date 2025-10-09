b51fea689f67a302b5b28ad5af3f9c0f57f7d4be
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-09 12:20:12 -0300

    chore:import Base from models.py

--- db.py
+++ db.py
@@ -1,8 +1,13 @@
 import os
 from sqlalchemy import create_engine, Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+from models import Base
 
 # Database setup
 engine = create_engine(f"postgresql://postgres:{os.getenv('POSTGRES_PASSWORD')}@db.bttijgbrforwaxpboawf.supabase.co:5432/postgres")
 SessionLocal = sessionmaker(bind=engine) 
 
+
+#DB initialization
+def init_db() -> None:
+    Base.metadata.create_all(engine)
