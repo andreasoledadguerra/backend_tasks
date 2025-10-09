5fa9df3fb2e73a6930e9670bea2e8fe68aa378fa
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-09 11:39:41 -0300

    feat: add database setup

--- db.py
+++ db.py
@@ -1,3 +1,8 @@
 import os
 from sqlalchemy import create_engine, Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+
+# Database setup
+engine = create_engine(f"postgresql://postgres:{os.getenv('POSTGRES_PASSWORD')}@db.bttijgbrforwaxpboawf.supabase.co:5432/postgres")
+SessionLocal = sessionmaker(bind=engine) 
+
