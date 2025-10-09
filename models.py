1e84d0c7db4d3b42912f1a5673cbe8c1d3d18a4c
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-09 12:16:11 -0300

    feat: add ORM models to models.py file

--- models.py
+++ models.py
@@ -1,3 +1,12 @@
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+# ORM Models
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50))
