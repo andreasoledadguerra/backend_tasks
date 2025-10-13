2f1269cbab1ef5071e6826afcf81a435a5d2901c
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-13 11:55:21 -0300

    feat: import libraries uvicorn, requests, FastAPI and BaseModel

--- main.py
+++ main.py
@@ -3,3 +3,13 @@
 #- La API debe poder crear un usuario en la base de datos.
 #- Eliminarlo.
 #- devolver un listado de todos los usuarios.
+
+#endpoints :
+#"/create_user", "/delete_user", "/get_users"Los endpoints seran:
+
+import uvicorn
+import requests
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
