fee2a3ec923faf0dbe7dd91f3eb58b0cc319cf1f
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-14 11:55:15 -0300

    refactor: import Session from sqlalchemy.orm and rename db parameter in function create_user()

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import uvicorn
 import requests
 
 from fastapi import FastAPI, Depends
-from sqlalchemy.orm import  sessionmaker
+from sqlalchemy.orm import  Session
 from db import get_session, init_db
 from models import User
 from pydantic import BaseModel
@@ -27,18 +27,18 @@ class UserRead(BaseModel):
     id: int
     name: str
 
-    class Config: #what is this?
+    class Config: # convierte el objeto ORM a un objeto Pydantic
         orm_mode = True
 
 
 # Definir el modelo de datos para crear un usuario (POST)
 @app.post("/create_user", response_model=UserRead)
-#def create_user(payload: UserCreate, db: Session = Depends(get_db)):
-#    new_user = User(name=payload.name)
-#    db.add(new_user)
-#    db.commit()
-#    db.refresh(new_user)
-#    return new_user
+def create_user(payload: UserCreate, db: Session = Depends(get_session)):
+    new_user = User(name=payload.name)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
 
 # Definir el modelo de datos para borrar un usuario (DELETE)
 @app.delete("/delete_user")
