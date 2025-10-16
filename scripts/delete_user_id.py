df5a8abaac24fde23061457d12bfe10ed5c38231
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-16 12:09:46 -0300

    refactor: update function delete_user()

--- scripts/delete_user_id.py
+++ scripts/delete_user_id.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 
 #@app.delete("/delete_user_id")
-def delete_user(user_id: int, db: Session = Depends(get_session)):
+def delete_user(user_id: int, db: Session = Depends(get_session))-> None:
     user = db.query(User).filter(User.id == user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
