6a27ce9ce5ef6bc9847c8a9d7059bb76cdcbc17f
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-16 12:08:10 -0300

    chore: add modules from fastapi, sqlalchemy.orm, db, models and pydantic

---
+++ scripts/delete_user_id.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import  Session
+from db import get_session
+from models import User
+from pydantic import BaseModel  
+
+
+#@app.delete("/delete_user_id")
+#def delete_user(user_id: int, db: Session = Depends(get_session)):
+#    user = db.query(User).filter(User.id == user_id).first()
+#    if not user:
+#        raise HTTPException(status_code=404, detail="User not found")
+#    db.delete(user)
+#    db.commit()     
+#    return None
