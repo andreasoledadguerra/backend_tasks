7ed73f7d6c43337e8d1f136c31562af07cfc8e5b
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-16 11:06:04 -0300

    fix: delete BASE_URL and url and library requests

--- scripts/create_user.py
+++ scripts/create_user.py
@@ -1,8 +1,22 @@
-import requests
 
-BASE_URL = "http://localhost:PORT"
-url = f"{BASE_URL}/create_user"
-#    
+#from fastapi import Depends
+#from sqlalchemy.orm import Session
+#
+#@app.post("/create_user", response_model=UserRead, status_code=201)
+#def create_user(payload: UserCreate, db: Session = Depends(get_session)) -> UserRead:
+#    # Crear la instancia ORM con los datos validados por Pydantic
+#    new_user = User(name=payload.name)
+#
+#    # Persistir en la DB
+#    db.add(new_user)
+#    db.commit()
+#    db.refresh(new_user)
+#
+#    # Devolver el objeto ORM; FastAPI lo serializa usando response_model (orm_mode=True)
+#    return new_user
+#
+
+    
 #def create_user(payload: UserCreate, db: Session = Depends(get_session)) -> UserRead:
 #    new_user = User(name=payload.name)
 #    db.add(new_user)
@@ -17,24 +31,3 @@ url = f"{BASE_URL}/create_user"
 #        status_code=response.status_code,
 #        success=response.status_code == 200)
 
-#--------------------EJEMPLO------------------------------------------
-## Decorador para definir un endpoint GET
-#@app.get("/temperature")
-#def get_temperature_by_dates(lat: float, lon: float, date_i: str, date_f: str) -> WeatherResponse:
-#    params = {
-#        "latitude": lat,
-#        "longitude": lon,
-#        "hourly": "temperature_2m",
-#        "start_date": date_i,
-#        "end_date": date_f,
-#        "timezone": "UTC"
-#    }
-#
-#    # Hace una petición a Open-Meteo
-#    response = requests.get(url_forecast, params=params)
-#    
-#    return WeatherResponse(
-#        data=response.json(),           # Extrae los datos JSON
-#        status_code=response.status_code,  # Copia el status code
-#        success=response.status_code == 200  # Marca si fue exitosa
-#    )
