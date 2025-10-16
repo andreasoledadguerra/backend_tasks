70a9fbdef40bb9e032bc8142cbaa68a8d2ff005f
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-16 12:01:27 -0300

    refactor: update hint typing and libraries.

--- scripts/get_user.py
+++ scripts/get_user.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from db import get_session
-from requests import UserCreate, UserRead
+from requests import UserRead
 from models import User
 
 #@app.get("/get_users", response_model=list[UserRead])
-def get_user(db: Session = Depends(get_session)):
+def get_user(db: Session = Depends(get_session)) -> list[UserRead]:
     users = db.query(User).all()
     return users
