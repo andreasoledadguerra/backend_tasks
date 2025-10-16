8ab186a62ac24a93a3a87e4d626a7c5bcfe54589
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-16 12:05:18 -0300

    refactor: add decorator to get_user()

--- scripts/get_user.py
+++ scripts/get_user.py
@@ -4,7 +4,7 @@ from db import get_session
 from requests import UserRead
 from models import User
 
-#@app.get("/get_users", response_model=list[UserRead])
+@app.get("/get_users", response_model=list[UserRead])
 def get_user(db: Session = Depends(get_session)) -> list[UserRead]:
     users = db.query(User).all()
     return users
