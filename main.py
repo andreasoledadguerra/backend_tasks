174ce20ec41e7716da7e472cf55fe34aaff16cf1
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-20 10:38:25 -0300

    refactor: extract snippet related to objects  UserCreate and UserRead

--- main.py
+++ main.py
@@ -12,17 +12,6 @@ from pydantic import BaseModel
 # Crear la instancia de FastAPI
 app = FastAPI()
 
-# Schema para crear y leer usuarios
-class UserCreate(BaseModel):
-    name: str
-    
-class UserRead(BaseModel):
-    id: int
-    name: str
-
-    class Config: # convierte el objeto ORM a un objeto Pydantic
-        orm_mode = True
-
 
 # Inicializar la base de datos al iniciar la aplicación
 @app.get("/")
