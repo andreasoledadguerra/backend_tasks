9dfe97170afaacf9b865b1ff41701fe57fb3f4a2
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-21 12:54:00 -0300

    refactor: rename endpoint

--- scripts/delete_user_id.py
+++ scripts/delete_user_id.py
@@ -1,6 +1,6 @@
 import requests
 
 if __name__ == "__main__":
-    response = requests.delete("http://localhost:8000/")
+    response = requests.delete("http://localhost:8000/delete_user_id", params={"user_id": 1})
     print(response.status_code)
     print(response.json())
