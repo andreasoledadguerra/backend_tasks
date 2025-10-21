b88708bb3722c20124bec7c32dcdd73dec67d333
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-21 12:53:21 -0300

    refactor: rename endpoint

--- scripts/create_user.py
+++ scripts/create_user.py
@@ -1,6 +1,6 @@
 import requests
 
 if __name__ == "__main__":
-    response = requests.post("http://localhost:8000/")
+    response = requests.post("http://localhost:8000/create_user", json={"name": "Test User"})
     print(response.status_code)
     print(response.json())
