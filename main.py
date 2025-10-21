bcaf9c492ee0e07b1360fb361e93b52c7ff6a30c
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-21 11:52:59 -0300

    docs: add comment for Uvicorn app startup

--- main.py
+++ main.py
@@ -64,6 +64,6 @@ def delete_user(user_id: int, db: Session = Depends(get_session)) -> list[UserRe
 
     return list[UserRead]
 
-
+# Ejecutar la aplicación con Uvicorn
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
