06abb131866b248a7547e46d4989f57662818717
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-22 12:29:39 -0300

    refactor: add variable date to the request to create a user

--- scripts/create_user.py
+++ scripts/create_user.py
@@ -1,6 +1,8 @@
 import requests
 
 if __name__ == "__main__":
-    response = requests.post("http://localhost:8000/create_user")
+    data = {"name": "Andy"}
+
+    response = requests.post("http://localhost:8000/create_user", json=data)
     print(response.status_code)
     print(response.json())
