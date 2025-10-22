6b60b05f79d2c6aede7b401b2001d4549ac60ca4
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-10-22 11:25:31 -0300

    refactor: update parameter in request.post()

--- scripts/create_user.py
+++ scripts/create_user.py
@@ -1,6 +1,6 @@
 import requests
 
 if __name__ == "__main__":
-    response = requests.post("http://localhost:8000/create_user", json={"name": "Test User"})
+    response = requests.post("http://localhost:8000/create_user")
     print(response.status_code)
     print(response.json())
